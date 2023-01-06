7bd1480e1bb03d48b96db23bdd35580a01cfe602
HyeBinYang <skdisk7368@gmail.com>
2023-01-07 01:26:41 +0900

    feat : devider 컴포넌트 구현

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Divider = () => {
-  return <div>Divider</div>;
+  return (
+    <div>
+      <hr css={{ boxSizing: "border-box", border: "none", borderBottom: "1px solid", color: "rgba(55,53,47,0.09)" }} />
+    </div>
+  );
 };
 
 export default Divider;
