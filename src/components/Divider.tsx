bd4991ebc945b21ef49e28ae41ccd653c856d3ba
HyeBinYang <skdisk7368@gmail.com>
2023-01-08 02:02:14 +0900

    feat : divider에 margin top, down 추가

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Divider = () => {
   return (
-    <div>
+    <div css={{ margin: "2em 0" }}>
       <hr css={{ boxSizing: "border-box", border: "none", borderBottom: "1px solid", color: "rgba(55,53,47,0.09)" }} />
     </div>
   );
