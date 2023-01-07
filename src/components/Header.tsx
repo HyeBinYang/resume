a9e8d57ce2fb24724e3b9a4c735170888ba98ab5
HyeBinYang <skdisk7368@gmail.com>
2023-01-07 12:30:59 +0900

    feat : Header에 다크모드 Icon 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,49 @@
 import React from "react";
+import { MdDarkMode } from "react-icons/md";
+import { BsFillSunFill } from "react-icons/bs";
 import styled from "@emotion/styled";
 
-const HeaderStyle = styled.header`
+const HeaderWrapper = styled.header`
   position: sticky;
   top: 0;
-  width: 100vw;
+  width: 100%;
   height: 80px;
 `;
 
+const HEADER_ICON_SIZE = 32;
+
 const Header = () => {
-  return <HeaderStyle>Header</HeaderStyle>;
+  return (
+    <HeaderWrapper>
+      {/* <button
+        css={{
+          backgroundColor: "skyblue",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        <MdDarkMode size={HEADER_ICON_SIZE} />
+      </button> */}
+      <button
+        css={{
+          padding: "8px 10px",
+          backgroundColor: "inherit",
+          border: "none",
+          borderRadius: "50%",
+          position: "absolute",
+          top: "50%",
+          right: 0,
+          transform: "translateY(-50%)",
+          cursor: "pointer",
+          ":hover": {
+            background: "rgba(0,0,0,0.05)",
+          },
+        }}
+      >
+        <BsFillSunFill size={HEADER_ICON_SIZE} />
+      </button>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
