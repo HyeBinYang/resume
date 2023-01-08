3ca107a078e2118df8cc2b2709fae281e99c602b
HyeBinYang <skdisk7368@gmail.com>
2023-01-09 00:15:52 +0900

    feat : meta 태그 - og 추가

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -18,6 +18,13 @@ const SEO = () => {
       <meta charSet="utf-8" />
       <title>{metaData.title}</title>
       <meta name="description" content={metaData.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://resume-robin.netlify.app/" />
+      <meta property="og:title" content={metaData.title} />
+      {/* <meta property="og:image" content="https://example.com/image.jpg" /> */}
+      <meta property="og:description" content={metaData.description} />
+      <meta property="og:site_name" content={metaData.title} />
+      <meta property="og:locale" content="ko_KR" />
       <link
         rel="icon"
         href={`data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>${metaData.favicon}</text></svg>`}
